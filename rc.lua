f384e204df6466c5183a3dcb8ef7e932ad012374
aymanfarhat <ayman.farhat1@gmail.com>
2013-05-30 22:57:34 +0300

    Added names tags

--- rc.lua
+++ rc.lua
@@ -74,10 +74,15 @@ layouts =
 
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
+tags = {
+  names  = { "term","code", "www", "ssh/ftp", "docs", "misc", 7, 8, 9 },
+  layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1],
+             layouts[6], layouts[5], layouts[5], layouts[5]
+}}
+
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag(tags.names, s,  tags.layout)
 end
 -- }}}
 
